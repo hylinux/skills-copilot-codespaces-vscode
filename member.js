cb80c406e56599ce35d07e6e17c8a02c4b0d3964
HongWei Guo <hongweig@163.com>
2023-07-18 03:51:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl'
+  };
+}
+
+    
